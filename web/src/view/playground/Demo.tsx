a4dab55dfcb1f0a20f273e4a51aed338b4cf8937
paroniriaa <44891450+paroniriaa@users.noreply.github.com>
2020-10-19 22:52:19 -0700

    Update Demo.tsx
    kim's framework

--- web/src/view/playground/Demo.tsx
+++ web/src/view/playground/Demo.tsx
@@ -1,6 +1,48 @@
 /* eslint-disable prettier/prettier */
 import * as React from 'react'
+import { Button } from '../../style/button'
+import { H1, H2 } from '../../style/header'
+import { Input } from '../../style/input'
+import { style } from '../../style/styled'
+import { Page } from '../page/Page'
 
 export function Demo() {
-  return <div> FK THIS SHIT </div>
+  return (
+    <Page>
+      <ST>
+        <H1>CS 188 - public chatting room</H1>
+        <H2>Please do not use imporoper language</H2>
+        <label></label>
+        <div id='textView' overflow-y='scroll'></div>
+        <tr>
+          <td width='90%'><Input type="text" id='input_text' placeholder='Say Hello to all'></Input></td>
+          <td width='10%'><Button type='submit' id='insert_text' onClick={temp}> Enter </Button></td>
+        </tr>
+
+
+      </ST>
+    </Page>
+
+  )
 }
+
+
+let line_num = 0
+function temp() {
+  const text = document.getElementById('textView')
+  const tex1 = document.getElementById('textView')?.innerHTML
+
+  const add = document.createElement('tr')
+  const blabla = document.getElementById('input_text')
+  console.log(tex1)
+  // console.log(blabla?.attributes, blabla?.attributes.length, blabla?.innerText)
+  add.textContent = "Line " + line_num + " :  TEST  " + blabla?.attributes[0].name + " " + blabla?.attributes[0].value
+  line_num = line_num + 1
+  text?.appendChild(add)
+}
+
+
+const ST = style('div', 'mb4 w-100 ba b--mid-gray br2 pa3 tc', {
+  borderLeftWidth: '20px',
+  borderRightWidth: '20px',
+})
