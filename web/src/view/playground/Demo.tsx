0af64c7bc045a3078d43f080f606d8aa9d486c40
paroniriaa <44891450+paroniriaa@users.noreply.github.com>
2020-10-20 20:39:35 -0700

    add textarea auto scroll

--- web/src/view/playground/Demo.tsx
+++ web/src/view/playground/Demo.tsx
@@ -11,38 +11,43 @@ export function Demo() {
     <Page>
       <ST>
         <H1>CS 188 - public chatting room</H1>
-        <H2>Please do not use imporoper language</H2>
+        <H2>Please do not use improper language</H2>
         <label></label>
-        <div id='textView' overflow-y='scroll'></div>
+        <S><th id="textView" align="left"></th></S>
         <tr>
-          <td width='90%'><Input type="text" id='input_text' placeholder='Say Hello to all'></Input></td>
-          <td width='10%'><Button type='submit' id='insert_text' onClick={temp}> Enter </Button></td>
+          <td width="90%">
+            <Input type="text" id="input_text" placeholder="Say Hello to all"></Input>
+          </td>
+          <td width="10%">
+            <Button type="submit" id="insert_text" onClick={temp}>
+              {' '}
+              Enter{' '}
+            </Button>
+          </td>
         </tr>
-
-
       </ST>
     </Page>
-
   )
 }
-
 
 let line_num = 0
 function temp() {
   const text = document.getElementById('textView')
-  const tex1 = document.getElementById('textView')?.innerHTML
-
   const add = document.createElement('tr')
-  const blabla = document.getElementById('input_text')
-  console.log(tex1)
-  // console.log(blabla?.attributes, blabla?.attributes.length, blabla?.innerText)
-  add.textContent = "Line " + line_num + " :  TEST  " + blabla?.attributes[0].name + " " + blabla?.attributes[0].value
+  const input = (document.getElementById('input_text') as HTMLInputElement).value
+
+  add.textContent = 'Line ' + line_num + ': ' + input + '\n'
   line_num = line_num + 1
   text?.appendChild(add)
 }
 
-
 const ST = style('div', 'mb4 w-100 ba b--mid-gray br2 pa3 tc', {
   borderLeftWidth: '20px',
   borderRightWidth: '20px',
+})
+
+const S = style('div', {
+  height: '300px',
+  width: '600px',
+  overflow: 'auto',
 })
