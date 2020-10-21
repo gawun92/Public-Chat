afb195dc7d6b971130e58d9b089cb6813798f66a
siyand6777 <49324620+siyand6777@users.noreply.github.com>
2020-10-20 22:16:23 -0700

    week1

--- web/src/view/playground/Demo.tsx
+++ web/src/view/playground/Demo.tsx
@@ -1,41 +1,60 @@
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
-import { ColorName, Colors } from '../../../../common/src/colors'
-import { H1, H3 } from '../../style/header'
-import { Spacer } from '../../style/spacer'
+import { Button } from '../../style/button'
+import { H1, H2 } from '../../style/header'
+import { Input } from '../../style/input'
 import { style } from '../../style/styled'
-import { IntroText } from '../../style/text'
 import { AppRouteParams } from '../nav/route'
 import { Page } from '../page/Page'
 
 interface DemoPageProps extends RouteComponentProps, AppRouteParams {}
 
 export function Demo(props: DemoPageProps){
-  return  <Page>
-            <Section>
-              <H1>CS 188</H1>
-              <H3>Scalable Internet Services</H3>
-              <H3>UCLA, Fall 2020</H3>
-              <Spacer $h4 />
-              <div> hi  </div>
-              <IntroText>Lecture slides and code will be posted regularly. Schedule subject to change.</IntroText>
-            </Section>
-          </Page>
+  return ( <Page>
+    <ST>
+      <H1>CS 188 - public chatting room</H1>
+      <H2>Please do not use improper language</H2>
+      <label></label>
+      <S><th id="textView" align="left"></th></S>
+      <tr>
+        <td width="90%">
+          <Input type="text" id="input_text" placeholder="Say Hello to all"></Input>
+        </td>
+        <td width="10%">
+          <Button type="submit" id="insert_text" onClick={temp}>
+            {' '}
+            Enter{' '}
+          </Button>
+        </td>
+      </tr>
+    </ST>
+  </Page>)
+
 }
 
-const Section = style('div', 'mb4 mid-gray ba b--mid-gray br2 pa3', (p: { $color?: ColorName }) => ({
-  borderLeftColor: Colors[p.$color || "ink"] + '!important',
-  borderLeftWidth: '10px',
-  borderRightColor: Colors[p.$color || "ink"] + '!important',
-  borderRightWidth: '10px',
-}))
+//const Content = style('div', 'flex-l w-400-l mr100-l')
 
-/*import { useQuery, useSubscription } from '@apollo/client'
-import { useLocation } from '@reach/router'
-import * as React from 'react'
-import { Fragment, useContext, useEffect, useState } from 'react'
-import { strutil } from '../../../../common/src/util'
-import { getApolloClient } from '../../graphql/apolloClient'
+const S = style('div',{
+  margin: 'auto',
+  height: '300px',
+  width: '500px',
+  overflowWrap: 'anywhere',
+  overflow: 'auto',
+})
 
-*/
+let line_num = 0
+function temp() {
+  const text = document.getElementById('textView')
+  const add = document.createElement('tr')
+  const input = (document.getElementById('input_text') as HTMLInputElement).value
 
+  add.textContent = 'Line ' + line_num + ': ' + input
+  line_num = line_num + 1
+  text?.appendChild(add)
+}
+
+const ST = style('div', 'mb4 w-100 ba b--mid-gray br2 pa3 tc', {
+  width:'600px',
+  borderLeftWidth: '20px',
+  borderRightWidth: '20px',
+})
